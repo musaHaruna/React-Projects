fcee5ca9662ed8abb6076a91a264e0ede8c470bc
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2023-08-17 07:29:33 -0700

    Movies and weather project done

---
+++ Weather/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App-v1";
+import Counter from "./Counter"
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Counter />
+  </React.StrictMode>
+);
