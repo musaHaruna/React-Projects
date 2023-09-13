a9cedc9cc9678631b7aa10afead6ea01567ee0f9
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2023-09-13 08:01:48 -0700

    add array paginate

--- Pagination/src/App.js
+++ Pagination/src/App.js
@@ -2,6 +2,16 @@ import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
 function App() {
+  const { loading, data } = useFetch()
+  const [page, setPage] = useState(4)
+  const [followers, setFollowers] = useState([])
+
+  useEffect(() => {
+    if (loading) return
+    setFollowers(data[page])
+  }, [loading, page])
+  console.log(data)
+
   return <h2>pagination starter</h2>
 }
 
--- Pagination/src/utils.js
+++ Pagination/src/utils.js
@@ -1,3 +1,14 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 10
+  const numberOfPages = Math.ceil(followers.length / itemsPerPage)
+  console.log(itemsPerPage)
+
+  const newFollowers = Array.from({ length: numberOfPages }, (_, index) => {
+    const start = index * itemsPerPage
+    return followers.slice(start, start + itemsPerPage)
+  })
+
+  return newFollowers
+}
 
 export default paginate
